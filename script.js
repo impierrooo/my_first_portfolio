e32517a76a811e28b741b9ed21fb1f829a35a5fc
impierrooo <90644687+impierrooo@users.noreply.github.com>
2022-05-15 15:11:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,282 @@
+/*Fonction changement de page*/
+
+function Page_introduction(){
+	document.getElementById('page_introduction').style.visibility = 'visible';
+	document.getElementById('page_créations').style.visibility = 'hidden';
+	document.getElementById('page_contact').style.visibility = 'hidden';
+	window.scrollTo(0,0);
+}
+
+function Page_créations(){
+	document.getElementById('page_introduction').style.visibility = 'hidden';
+	document.getElementById('page_créations').style.visibility = 'visible';
+	document.getElementById('page_contact').style.visibility = 'hidden';
+	window.scrollTo(0,0);
+}
+
+function Page_contact(){
+	document.getElementById('page_introduction').style.visibility = 'hidden';
+	document.getElementById('page_créations').style.visibility = 'hidden';
+	document.getElementById('page_contact').style.visibility = 'visible';
+	window.scrollTo(0,0);
+}
+
+/*Fonction changement de couleurs*/
+
+function ColorWhite(){
+	document.getElementById("title_text1").setAttribute("style", "color: #FFFFFF"); 
+	document.getElementById("title_text2").setAttribute("style", "color: #FFFFFF"); 
+	document.getElementById("title_text3").setAttribute("style", "color: #FFFFFF"); 
+	document.getElementById("title_text4").setAttribute("style", "color: #FFFFFF"); 
+	document.getElementById("title_text5").setAttribute("style", "color: #FFFFFF"); 
+	document.getElementById("title_text6").setAttribute("style", "color: #FFFFFF"); 
+	document.getElementById("title_text7").setAttribute("style", "color: #FFFFFF"); 
+	document.getElementById("title_text8").setAttribute("style", "color: #FFFFFF"); 
+	document.getElementById("title_text9").setAttribute("style", "color: #FFFFFF"); 
+	document.getElementById("title_text10").setAttribute("style", "color: #FFFFFF"); 
+	document.getElementById("title_text11").setAttribute("style", "color: #FFFFFF"); 
+	document.getElementById("title_text12").setAttribute("style", "color: #FFFFFF"); 
+	document.getElementById("title_text13").setAttribute("style", "color: #FFFFFF"); 
+	document.getElementById("title_text14").setAttribute("style", "color: #FFFFFF"); 
+	document.getElementById("title_text15").setAttribute("style", "color: #FFFFFF"); 
+
+	document.getElementById("typing_slider_text1").setAttribute("style", "color: #FFFFFF"); 
+
+	document.getElementById("paragraphe_text1").setAttribute("style", "color: #FFFFFF"); 
+}
+
+function ColorYellow(){
+	document.getElementById("title_text1").setAttribute("style", "color: #e4b196"); 
+	document.getElementById("title_text2").setAttribute("style", "color: #e4b196"); 
+	document.getElementById("title_text3").setAttribute("style", "color: #e4b196"); 
+	document.getElementById("title_text4").setAttribute("style", "color: #e4b196"); 
+	document.getElementById("title_text5").setAttribute("style", "color: #e4b196"); 
+	document.getElementById("title_text6").setAttribute("style", "color: #e4b196"); 
+	document.getElementById("title_text7").setAttribute("style", "color: #e4b196"); 
+	document.getElementById("title_text8").setAttribute("style", "color: #e4b196"); 
+	document.getElementById("title_text9").setAttribute("style", "color: #e4b196"); 
+	document.getElementById("title_text10").setAttribute("style", "color: #e4b196"); 
+	document.getElementById("title_text11").setAttribute("style", "color: #e4b196"); 
+	document.getElementById("title_text12").setAttribute("style", "color: #e4b196"); 
+	document.getElementById("title_text13").setAttribute("style", "color: #e4b196"); 
+	document.getElementById("title_text14").setAttribute("style", "color: #e4b196"); 
+	document.getElementById("title_text15").setAttribute("style", "color: #e4b196"); 
+
+	document.getElementById("typing_slider_text1").setAttribute("style", "color: #e4b196"); 
+
+	document.getElementById("paragraphe_text1").setAttribute("style", "color: #e4b196"); 
+}
+
+function ColorPurple(){
+	document.getElementById("title_text1").setAttribute("style", "color: #845cd6"); 
+	document.getElementById("title_text2").setAttribute("style", "color: #845cd6"); 
+	document.getElementById("title_text3").setAttribute("style", "color: #845cd6"); 
+	document.getElementById("title_text4").setAttribute("style", "color: #845cd6"); 
+	document.getElementById("title_text5").setAttribute("style", "color: #845cd6"); 
+	document.getElementById("title_text6").setAttribute("style", "color: #845cd6"); 
+	document.getElementById("title_text7").setAttribute("style", "color: #845cd6"); 
+	document.getElementById("title_text8").setAttribute("style", "color: #845cd6"); 
+	document.getElementById("title_text9").setAttribute("style", "color: #845cd6"); 
+	document.getElementById("title_text10").setAttribute("style", "color: #845cd6"); 
+	document.getElementById("title_text11").setAttribute("style", "color: #845cd6"); 
+	document.getElementById("title_text12").setAttribute("style", "color: #845cd6"); 
+	document.getElementById("title_text13").setAttribute("style", "color: #845cd6"); 
+	document.getElementById("title_text14").setAttribute("style", "color: #845cd6"); 
+	document.getElementById("title_text15").setAttribute("style", "color: #845cd6"); 
+
+	document.getElementById("typing_slider_text1").setAttribute("style", "color: #845cd6"); 
+
+	document.getElementById("paragraphe_text1").setAttribute("style", "color: #845cd6"); 
+}
+
+function ColorBlue(){
+	document.getElementById("title_text1").setAttribute("style", "color: #3a80bb"); 
+	document.getElementById("title_text2").setAttribute("style", "color: #3a80bb"); 
+	document.getElementById("title_text3").setAttribute("style", "color: #3a80bb"); 
+	document.getElementById("title_text4").setAttribute("style", "color: #3a80bb"); 
+	document.getElementById("title_text5").setAttribute("style", "color: #3a80bb"); 
+	document.getElementById("title_text6").setAttribute("style", "color: #3a80bb"); 
+	document.getElementById("title_text7").setAttribute("style", "color: #3a80bb"); 
+	document.getElementById("title_text8").setAttribute("style", "color: #3a80bb"); 
+	document.getElementById("title_text9").setAttribute("style", "color: #3a80bb"); 
+	document.getElementById("title_text10").setAttribute("style", "color: #3a80bb"); 
+	document.getElementById("title_text11").setAttribute("style", "color: #3a80bb"); 
+	document.getElementById("title_text12").setAttribute("style", "color: #3a80bb"); 
+	document.getElementById("title_text13").setAttribute("style", "color: #3a80bb"); 
+	document.getElementById("title_text14").setAttribute("style", "color: #3a80bb"); 
+	document.getElementById("title_text15").setAttribute("style", "color: #3a80bb");  
+
+	document.getElementById("typing_slider_text1").setAttribute("style", "color: #3a80bb"); 
+
+	document.getElementById("paragraphe_text1").setAttribute("style", "color: #3a80bb");
+}
+
+function ColorRed(){
+	document.getElementById("title_text1").setAttribute("style", "color: #d3364b"); 
+	document.getElementById("title_text2").setAttribute("style", "color: #d3364b"); 
+	document.getElementById("title_text3").setAttribute("style", "color: #d3364b"); 
+	document.getElementById("title_text4").setAttribute("style", "color: #d3364b"); 
+	document.getElementById("title_text5").setAttribute("style", "color: #d3364b"); 
+	document.getElementById("title_text6").setAttribute("style", "color: #d3364b"); 
+	document.getElementById("title_text7").setAttribute("style", "color: #d3364b"); 
+	document.getElementById("title_text8").setAttribute("style", "color: #d3364b"); 
+	document.getElementById("title_text9").setAttribute("style", "color: #d3364b"); 
+	document.getElementById("title_text10").setAttribute("style", "color: #d3364b"); 
+	document.getElementById("title_text11").setAttribute("style", "color: #d3364b"); 
+	document.getElementById("title_text12").setAttribute("style", "color: #d3364b"); 
+	document.getElementById("title_text13").setAttribute("style", "color: #d3364b"); 
+	document.getElementById("title_text14").setAttribute("style", "color: #d3364b"); 
+	document.getElementById("title_text15").setAttribute("style", "color: #d3364b");  
+
+	document.getElementById("typing_slider_text1").setAttribute("style", "color: #d3364b");
+
+	document.getElementById("paragraphe_text1").setAttribute("style", "color: #d3364b"); 
+}
+
+function ColorGreen(){
+	document.getElementById("title_text1").setAttribute("style", "color: #2effbd"); 
+	document.getElementById("title_text2").setAttribute("style", "color: #2effbd"); 
+	document.getElementById("title_text3").setAttribute("style", "color: #2effbd"); 
+	document.getElementById("title_text4").setAttribute("style", "color: #2effbd"); 
+	document.getElementById("title_text5").setAttribute("style", "color: #2effbd"); 
+	document.getElementById("title_text6").setAttribute("style", "color: #2effbd"); 
+	document.getElementById("title_text7").setAttribute("style", "color: #2effbd"); 
+	document.getElementById("title_text8").setAttribute("style", "color: #2effbd"); 
+	document.getElementById("title_text9").setAttribute("style", "color: #2effbd"); 
+	document.getElementById("title_text10").setAttribute("style", "color: #2effbd"); 
+	document.getElementById("title_text11").setAttribute("style", "color: #2effbd"); 
+	document.getElementById("title_text12").setAttribute("style", "color: #2effbd"); 
+	document.getElementById("title_text13").setAttribute("style", "color: #2effbd"); 
+	document.getElementById("title_text14").setAttribute("style", "color: #2effbd"); 
+	document.getElementById("title_text15").setAttribute("style", "color: #2effbd");  
+
+	document.getElementById("typing_slider_text1").setAttribute("style", "color: #2effbd");
+
+	document.getElementById("paragraphe_text1").setAttribute("style", "color: #2effbd"); 
+}
+
+function ColorPink(){
+	document.getElementById("title_text1").setAttribute("style", "color: #d324c9"); 
+	document.getElementById("title_text2").setAttribute("style", "color: #d324c9"); 
+	document.getElementById("title_text3").setAttribute("style", "color: #d324c9"); 
+	document.getElementById("title_text4").setAttribute("style", "color: #d324c9"); 
+	document.getElementById("title_text5").setAttribute("style", "color: #d324c9"); 
+	document.getElementById("title_text6").setAttribute("style", "color: #d324c9"); 
+	document.getElementById("title_text7").setAttribute("style", "color: #d324c9"); 
+	document.getElementById("title_text8").setAttribute("style", "color: #d324c9"); 
+	document.getElementById("title_text9").setAttribute("style", "color: #d324c9"); 
+	document.getElementById("title_text10").setAttribute("style", "color: #d324c9"); 
+	document.getElementById("title_text11").setAttribute("style", "color: #d324c9"); 
+	document.getElementById("title_text12").setAttribute("style", "color: #d324c9"); 
+	document.getElementById("title_text13").setAttribute("style", "color: #d324c9"); 
+	document.getElementById("title_text14").setAttribute("style", "color: #d324c9"); 
+	document.getElementById("title_text15").setAttribute("style", "color: #d324c9");  
+
+	document.getElementById("typing_slider_text1").setAttribute("style", "color: #d324c9");
+
+	document.getElementById("paragraphe_text1").setAttribute("style", "color: #d324c9"); 
+}
+
+/*Système de traduction*/
+
+function langage_france(){
+	document.getElementById("sidebar_top_text2").innerHTML = "Contact";
+	document.getElementById("sidebar_top_text3").innerHTML = "Créations";
+	document.getElementById("sidebar_top_text4").innerHTML = "Introduction";
+
+	document.getElementById("sidebar_top_text5").innerHTML = "Contact";
+	document.getElementById("sidebar_top_text6").innerHTML = "Créations";
+	document.getElementById("sidebar_top_text7").innerHTML = "Introduction";
+
+	document.getElementById("sidebar_top_text8").innerHTML = "Contact";
+	document.getElementById("sidebar_top_text9").innerHTML = "Créations";
+	document.getElementById("sidebar_top_text10").innerHTML = "Introduction";
+
+	document.getElementById("title_text1").innerHTML = "Je suis";
+	document.getElementById("title_text2").innerHTML = "&nbsp;D";
+	document.getElementById("title_text3").innerHTML = "é";
+	document.getElementById("title_text4").innerHTML = "v";
+	document.getElementById("title_text5").innerHTML = "e";
+	document.getElementById("title_text6").innerHTML = "l";
+	document.getElementById("title_text7").innerHTML = "o";
+	document.getElementById("title_text8").innerHTML = "p";
+	document.getElementById("title_text9").innerHTML = "p";
+	document.getElementById("title_text10").innerHTML = "e";
+	document.getElementById("title_text11").innerHTML = "u";
+	document.getElementById("title_text12").innerHTML = "r";
+	document.getElementById("title_text13").innerHTML = "&nbsp;W";
+	document.getElementById("title_text14").innerHTML = "e";
+	document.getElementById("title_text15").innerHTML = "b";
+
+	document.getElementById("paragraphe_text1").innerHTML = "Aucune barrière n'est infranchissable !&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+
+	document.getElementById("typing_slider_text1").innerHTML = "Je maîtrise le langage";
+}
+
+function langage_angleterre(){
+	document.getElementById("sidebar_top_text2").innerHTML = "Contact";
+	document.getElementById("sidebar_top_text3").innerHTML = "Créations";
+	document.getElementById("sidebar_top_text4").innerHTML = "Introduction";
+
+	document.getElementById("sidebar_top_text5").innerHTML = "Contact";
+	document.getElementById("sidebar_top_text6").innerHTML = "Créations";
+	document.getElementById("sidebar_top_text7").innerHTML = "Introduction";
+
+	document.getElementById("sidebar_top_text8").innerHTML = "Contact";
+	document.getElementById("sidebar_top_text9").innerHTML = "Créations";
+	document.getElementById("sidebar_top_text10").innerHTML = "Introduction";
+
+	document.getElementById("title_text1").innerHTML = "I am a";
+	document.getElementById("title_text2").innerHTML = "&nbsp;W";
+	document.getElementById("title_text3").innerHTML = "e";
+	document.getElementById("title_text4").innerHTML = "b";
+	document.getElementById("title_text5").innerHTML = "&nbsp;";
+	document.getElementById("title_text6").innerHTML = "D";
+	document.getElementById("title_text7").innerHTML = "e";
+	document.getElementById("title_text8").innerHTML = "v";
+	document.getElementById("title_text9").innerHTML = "e";
+	document.getElementById("title_text10").innerHTML = "l";
+	document.getElementById("title_text11").innerHTML = "o";
+	document.getElementById("title_text12").innerHTML = "p";
+	document.getElementById("title_text13").innerHTML = "e";
+	document.getElementById("title_text14").innerHTML = "r";
+	document.getElementById("title_text15").innerHTML = "";
+
+	document.getElementById("paragraphe_text1").innerHTML = "No barrier is insurmountable !&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+
+	document.getElementById("typing_slider_text1").innerHTML = "I am fluent in language";
+}
+
+function langage_espagne(){
+	document.getElementById("sidebar_top_text2").innerHTML = "Contactar";
+	document.getElementById("sidebar_top_text3").innerHTML = "Creaciones";
+	document.getElementById("sidebar_top_text4").innerHTML = "Introducción";
+
+	document.getElementById("sidebar_top_text5").innerHTML = "Contactar";
+	document.getElementById("sidebar_top_text6").innerHTML = "Creaciones";
+	document.getElementById("sidebar_top_text7").innerHTML = "Introducción";
+
+	document.getElementById("sidebar_top_text8").innerHTML = "Contactar";
+	document.getElementById("sidebar_top_text9").innerHTML = "Creaciones";
+	document.getElementById("sidebar_top_text10").innerHTML = "Introducción";
+
+	document.getElementById("title_text1").innerHTML = "Soy un";
+	document.getElementById("title_text2").innerHTML = "&nbsp;D";
+	document.getElementById("title_text3").innerHTML = "e";
+	document.getElementById("title_text4").innerHTML = "s";
+	document.getElementById("title_text5").innerHTML = "a";
+	document.getElementById("title_text6").innerHTML = "rr";
+	document.getElementById("title_text7").innerHTML = "o";
+	document.getElementById("title_text8").innerHTML = "ll";
+	document.getElementById("title_text9").innerHTML = "a";
+	document.getElementById("title_text10").innerHTML = "d";
+	document.getElementById("title_text11").innerHTML = "o";
+	document.getElementById("title_text12").innerHTML = "r";
+	document.getElementById("title_text13").innerHTML = "&nbsp;W";
+	document.getElementById("title_text14").innerHTML = "e";
+	document.getElementById("title_text15").innerHTML = "b";
+
+	document.getElementById("paragraphe_text1").innerHTML = "¡Ninguna barrera es insuperable!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+
+	document.getElementById("typing_slider_text1").innerHTML = "Yo domino el lenguaje";
+}
